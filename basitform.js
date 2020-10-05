4e5ecf0cac885b1402385e371e3c4a172d02b036
Fevziyildizhan <70893053+Fevziyildizhan@users.noreply.github.com>
2020-10-05 21:01:52 +0300

    Create basitform.js

---
+++ basitform.js
@@ -0,0 +1,20 @@
+var adi = String( prompt('Adınızı girin'))
+var sifre = (prompt('şifre giriniz'))
+
+if(adi==''){
+    
+}else if(adi.length<=5){
+    console.log('lütfen adınız 5 kelimeli olsun')
+}else{
+    console.log('doğru')
+}
+
+if(sifre==' '){
+
+}else if(sifre.length<=5){
+      console.log('5 haneli yada 5 kelimeli şifre girin')
+}else{
+    console.log('hoşgeldin')
+
+
+}
